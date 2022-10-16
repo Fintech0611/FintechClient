107498d00b034d6db3b89b22a135a3c1a27e01da
QIN-HAIFENG <qinhaifengnc@gmail.com>
2022-10-16 16:21:52 +0900

    change project name.

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -29,10 +29,17 @@ namespace fin_tech_stock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmMain";
+            this.Text = "FinTechStock";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
